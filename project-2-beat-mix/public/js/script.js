a99a40ab9b42db7e487552e82f42733c8917ec93
r.vazquez@meditecsols.com <ramirovazquez95@gmail.com>
2018-01-13 10:33:44 -0600

    el primer commit

---
+++ project-2-beat-mix/public/js/script.js
@@ -0,0 +1,6 @@
+// Drum Arrays
+let kicks = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+let snares = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+let hiHats= [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+let rideCymbals = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+
