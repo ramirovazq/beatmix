5a1cb5ec29bd1876f4bbc75292e8a06ff53bbcec
r.vazquez@meditecsols.com <r.vazquez@meditecsols.com>
2018-01-16 20:49:49 -0600

    se resuelven varias funciones

--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -1,6 +1,82 @@
 // Drum Arrays
-let kicks = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
-let snares = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
-let hiHats= [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
-let rideCymbals = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+
+const arrayvacio = () => Array(16).fill(false);
+
+let kicks       = arrayvacio();
+let snares      = arrayvacio();
+let hiHats      = arrayvacio();
+let rideCymbals = arrayvacio();
+
+const equivalencia = (nombre) => {
+  switch (nombre) {
+    case 'kicks':
+      return kicks;
+    case 'snares':
+      return snares;
+    case 'hiHats':
+      return hiHats;
+    case 'rideCymbals':
+      return rideCymbals;
+    default:
+      return;
+  }
+}
+
+
+function toggleDrum(el_array, indice){
+
+    if(typeof(el_array) === 'string'){
+        el_array = equivalencia(el_array)
+    }
+
+    if(indice && (indice <= el_array.length && !(indice < 0))){
+        if (el_array[indice]){
+            el_array[indice] = false;
+        }else{
+            el_array[indice] = true;
+        }
+
+    }else{
+        if(indice===0){
+            if(el_array[indice]){
+            el_array[indice] = false;
+            }else{
+            el_array[indice] = true;
+            }
+        }
+    }
+
+
+} // toggleDrum
+
+
+function clear(el_array){
+
+    if(typeof(el_array) === 'string'){
+        el_array = equivalencia(el_array)
+    }
+
+    if(el_array){
+        el_array.fill(false)
+    }
+} 
+
+
+
+
+function invierte(valor){
+    return !valor;
+}
+
+function invert(el_array){
+    if(typeof(el_array) === 'string'){
+        el_array = equivalencia(el_array)
+    }
+
+    if(el_array){
+        for (let j=0; el_array.length > j ; j++){
+            el_array[j] = invierte(el_array[j]);
+        }
+    }
+} 
 
